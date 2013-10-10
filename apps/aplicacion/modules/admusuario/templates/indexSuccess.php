a6a07c6d80592e906df18ae5aba0864acbdafd7d
icallapa <callapaivan@gmail.com>
2013-10-10 18:48:14 -0400

    Template y componentes del menu - Nuevo esquema

---
+++ apps/aplicacion/modules/admusuario/templates/indexSuccess.php
@@ -0,0 +1,45 @@
+<!-- BEGIN PAGE CONTENT-->
+<div class="row" style="padding: 20px;">
+   <div class="col-md-12">
+      <!-- BEGIN EXAMPLE TABLE PORTLET-->
+      <div class="portlet box green">
+         <div class="portlet-title">
+            <div class="caption">
+                <i class="icon-user-md"></i>
+                Lista de Usuario    
+            </div>
+         </div>
+         <div class="portlet-body">
+            <div id="sample_1_wrapper" class="dataTables_wrapper form-inline" role="grid">
+               <table aria-describedby="sample_1_info" class="table table-striped table-bordered table-hover table-full-width dataTable" id="sample_1">
+               <thead>
+                  <tr role="row">
+                      <th tabindex="0">Rendering engine</th>
+                      <th tabindex="0">Rendering engine</th>
+                      <th tabindex="0">Rendering engine</th>
+                      <th tabindex="0">Rendering engine</th>
+                      <th tabindex="0">Rendering engine</th>
+               </thead>
+               <tbody aria-relevant="all" aria-live="polite" role="alert">
+                  <tr class="odd">
+                     <td class="">Webkit</td>
+                     <td class=" ">Safari 2.0</td>
+                     <td class="hidden-xs  sorting_1">OSX.4+</td>
+                     <td class="hidden-xs ">419.3</td>
+                     <td class="hidden-xs ">A</td>
+                  </tr><tr class="even">
+                     <td class="">Webkit</td>
+                     <td class=" ">Safari 3.0</td>
+                     <td class="hidden-xs  sorting_1">OSX.4+</td>
+                     <td class="hidden-xs ">522.1</td>
+                     <td class="hidden-xs ">A</td>
+                  </tr>
+               </tbody>
+               </table>
+            </div>
+         </div>
+      </div>
+      <!-- END EXAMPLE TABLE PORTLET-->
+   </div>
+</div>
+<!-- END PAGE CONTENT-->
